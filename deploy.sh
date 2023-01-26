66344fef6dec7b8aa18472dd1fe3561e1868e245
Rounak Vyas <itsron143@gmail.com>
2023-01-26 15:37:57 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -8,33 +8,33 @@ echo "Creating the volume..."
 
 echo "checking if /opt/postgres/data is created or not..."
 if [ -d /opt/postgres/data ]; then
-  echo "/opt/postgres/data does exist."
-else
-  echo "creating /opt/postgres/data ..."
-  sudo mkdir -p /opt/postgres/data
+  echo "/opt/postgres/data does exist, removing it..."
+  sudo rm -rf /opt/postgres/data
 fi
+
+echo "creating /opt/postgres/data ..."
+sudo mkdir -p /opt/postgres/data
 
 microk8s kubectl apply -f ./kubernetes/postgres/postgres-storage.yaml
 
 echo "Creating the database credentials..."
-
 microk8s kubectl apply -f ./kubernetes/postgres/postgres-secret.yaml
 
 echo "Creating the postgres deployment and service..."
-
 microk8s kubectl create -f ./kubernetes/postgres/postgres-deployment.yaml
 microk8s kubectl create -f ./kubernetes/postgres/postgres-service.yaml
 
-echo "Waiting for postgres..."
+# while ! nc -z postgres 5432; do
+#   sleep 0.1
+# done
 
-while ! nc -z postgres 5432; do
-  sleep 0.1
-done
+echo "waiting 10 seconds for postgres to start..."
+sleep 10
 
-echo "PostgreSQL started"
+echo "Check pods info to see if postgres has begun.."
+microk8s kubectl get pods
 
 echo "Creating the flask deployment and service..."
-
 microk8s kubectl create -f ./kubernetes/flask/flask-deployment.yaml
 microk8s kubectl create -f ./kubernetes/flask/flask-service.yaml
 

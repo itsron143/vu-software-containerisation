afaa1deb1f499b77fa1157350208fd73c742dcb4
kemalshekerbackup <kemalsheker@gmail.com>
2023-01-26 17:45:05 +0100

    removal deploy

--- kubernetes_removal_script.sh
+++ kubernetes_removal_script.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
-microk8s kubectl delete all --all --all-namespaces
+microk8s kubectl delete pod --all
 microk8s kubectl delete pvc --all
 microk8s kubectl delete pv --all
+microk8s kubectl delete --all deployments
+microk8s kubectl delete svc postgres
+microk8s kubectl delete svc pushups-logger

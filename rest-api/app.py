cc4230d087919eb8eadb8921efd106b24a2cd7a2
Rounak Vyas <itsron143@gmail.com>
2023-01-31 11:16:56 +0100

    clean ups

--- rest-api/app.py
+++ rest-api/app.py
@@ -3,7 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://postgres:postgres@localhost:5432/people"
+app.config['SQLALCHEMY_DATABASE_URI'] = 
+"postgresql://postgres:postgres@postgres:5432/people"
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
 

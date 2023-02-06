c2046aa20d37d0957d980e1e9fe32b5544e3d9de
Rounak Vyas <itsron143@gmail.com>
2023-02-06 20:24:19 +0100

    updated deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,18 +1,27 @@
 #!/bin/bash
- 
-echo "Enable microk8s DNS"
-microk8s enable dns
 
-echo "Enable microk8s RBAC"
-microk8s enable rbac
- 
+
 echo "Setting postgres config..."
- 
+
 microk8s kubectl apply -f ./kubernetes/postgres/postgres-config.yaml
- 
+
 echo "Creating the database credentials..."
 microk8s kubectl apply -f ./kubernetes/postgres/postgres-secret.yaml
- 
+
+echo "Creating the volume..."
+
+microk8s kubectl apply -f ./kubernetes/postgres/postgres-storage.yaml
+
+echo "Creating the postgres deployment and service..."
+microk8s kubectl apply -f ./kubernetes/postgres/postgres-deployment.yaml
+microk8s kubectl apply -f ./kubernetes/postgres/postgres-service.yaml
+
+echo "waiting 10 seconds for postgres to start..."
+sleep 10
+
+echo "Check pods info to see if postgres has begun.."
+microk8s kubectl get pods
+
 echo "Creating the database networkpolicy..."
 microk8s kubectl apply -f ./kubernetes/postgres/postgres-network-policy.yaml
 
@@ -21,7 +30,6 @@ microk8s kubectl apply -f ./kubernetes/roles/pod-role.yaml
 
 echo "Creating the binding role for pods..."
 microk8s kubectl apply -f ./kubernetes/roles/pod-role-binding.yaml
-
 
 echo "Creating the role for volumes..."
 microk8s kubectl apply -f ./kubernetes/roles/volumes-role.yaml
@@ -35,31 +43,7 @@ microk8s kubectl apply -f ./kubernetes/roles/secrets-role.yaml
 echo "Creating the binding role for secrets..."
 microk8s kubectl apply -f ./kubernetes/roles/secrets-role-binding.yaml
 
-echo "Creating the volume..."
- 
-echo "checking if /opt/postgres/data is created or not..."
-if [ -d /opt/postgres/data ]; then
-  echo "/opt/postgres/data does exist, removing it..."
-  sudo rm -rf /opt/postgres/data
-fi
- 
-echo "creating /opt/postgres/data ..."
-sudo mkdir -p /opt/postgres/data
- 
-microk8s kubectl apply -f ./kubernetes/postgres/postgres-storage.yaml
- 
-echo "Creating the postgres deployment and service..."
-microk8s kubectl create -f ./kubernetes/postgres/postgres-deployment.yaml
-microk8s kubectl create -f ./kubernetes/postgres/postgres-service.yaml
- 
-echo "waiting 10 seconds for postgres to start..."
-sleep 10
- 
-echo "Check pods info to see if postgres has begun.."
-microk8s kubectl get pods
- 
 echo "Creating the flask deployment and service..."
-microk8s kubectl apply -f ./kubernetes/flask/flask-secret.yaml
 microk8s kubectl create -f ./kubernetes/flask/flask-deployment.yaml
 microk8s kubectl create -f ./kubernetes/flask/flask-service.yaml
 microk8s kubectl create -f ./kubernetes/flask/flask-network-policy.yaml
@@ -67,20 +51,18 @@ microk8s kubectl create -f ./kubernetes/flask/flask-ingress.yaml
 
 echo "Waiting 10 secs for flask to begin..."
 sleep 10
- 
+
 echo "Check pods again to see if rest-api pod is running..."
 microk8s kubectl get pods
 
 echo "Creating web-frontend deployment and service..."
+microk8s kubectl apply -f ./kubernetes/web-frontend/web-frontend-managed-cert.yaml
 microk8s kubectl apply -f ./kubernetes/web-frontend/web-deployment.yaml
 microk8s kubectl apply -f ./kubernetes/web-frontend/web-service.yaml
 microk8s kubectl apply -f ./kubernetes/web-frontend/web-network-policy.yaml
 microk8s kubectl apply -f ./kubernetes/web-frontend/web-ingress.yaml
 
-echo "Issuing TLS certificates..."
-microk8s kubectl apply -f ./kubernetes/clusterissuer.yaml
-
-echo "Waiting 10 seconds for web-front service to start..."
+echo "Wait for web-frontend pods to show up..."
 sleep 10
 
 echo "Check pods again to see if web-frontend pod is running..."
